ebb99de59ae29d883bd8c951fea382132fd9e067
Jorge Rodriguez <jarf4321@gmail.com>
2025-09-22 22:10:33 -0600

    Fix: stock layout

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let arrayProductos = [
+  { id: 1, nombre: "Producto 1", precio: 100, stock: 10 },
+  { id: 2, nombre: "Producto 2", precio: 200, stock: 5 },
+  { id: 3, nombre: "Producto 3", precio: 150, stock: 8 },
+  { id: 4, nombre: "Producto 4", precio: 300, stock: 2 },
+  { id: 5, nombre: "Producto 5", precio: 250, stock: 0 },
+]
